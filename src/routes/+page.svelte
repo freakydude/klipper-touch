a54f2c02f316045a64b991607b111f8f719fb568
FreakyDude <developer@freakydu.de>
2023-10-09 00:38:38 +0200

    Playing with design part 3
    Signed-off-by: Frank Roth developer@freakydu.de

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
+  import { Toolhead } from './../lib/moonraker/modules/Toolhead.ts';
   import { goto } from '$app/navigation';
   import { client, moonraker } from '$lib/base.svelte';
   import { JsonRpcRequest } from '$lib/jsonrpc/types/JsonRpcRequest';
   import { faSquareFull } from '@fortawesome/free-regular-svg-icons';
-  import { faPause, faStop, faSkull, faGear, faDownLong, faArrowsUpDown } from '@fortawesome/free-solid-svg-icons';
+  import { faPause, faStop, faSkull, faGear, faDownLong, faArrowsUpDown, faArrowsUpDownLeftRight } from '@fortawesome/free-solid-svg-icons';
   import Fa from 'svelte-fa';
 
   let toolheadPosition = moonraker.toolhead.Position;
@@ -86,7 +87,7 @@
   }
 </script>
 
-<div class="flex w-full flex-col flex-wrap items-stretch gap-2 border-t-2 border-green-400 bg-neutral-800 p-1">
+<div class="flex w-full flex-col flex-wrap items-stretch gap-2 border-t border-green-400 bg-neutral-800 p-1">
   <div class="flex flex-grow flex-col flex-wrap items-stretch justify-start gap-1 p-1">
     <button class="flex rounded border-l-4 border-red-400 bg-neutral-700 px-1 py-2 text-white hover:bg-neutral-500" on:click={() => goto('/temp_nozzle')}>
       <div class="self-center px-1"><Fa icon={faDownLong} /></div>
@@ -113,35 +114,35 @@
   </div>
   <div class="flex flex-grow flex-col flex-wrap items-stretch justify-between gap-1 p-1">
     <div class="flex border-b-2 border-neutral-600 px-1 py-1 text-white">
-      <div class="px-1">Layer</div>
+      <div class="px-1 text-green-400">Layer</div>
       <div class="flex-grow pl-2 pr-1 text-end">10 / 2300</div>
     </div>
     <div class="flex border-b-2 border-neutral-600 px-1 py-1 text-white">
-      <div class="px-1">Z</div>
+      <div class="px-1 text-green-400">Z</div>
       <div class="flex-grow pl-2 pr-1 text-end">{$toolheadPosition[2].toFixed(2)} mm</div>
     </div>
     <div class="flex border-b-2 border-neutral-600 px-1 py-1 text-white">
-      <div class="px-1">Filament</div>
+      <div class="px-1 text-green-400">Filament</div>
       <div class="flex-grow pl-2 pr-1 text-end">67.4 m</div>
     </div>
     <div class="flex border-b-2 border-neutral-600 px-1 py-1 text-white">
-      <div class="px-1">Printed</div>
+      <div class="px-1 text-green-400">Printed</div>
       <div class="flex-grow pl-2 pr-1 text-end">{$printStatsPrintDuration.toFixed(1)}</div>
     </div>
     <div class="flex border-b-2 border-neutral-600 px-1 py-1 text-white">
-      <div class="px-1">Estimation</div>
+      <div class="px-1 text-green-400">Estimation</div>
       <div class="flex-grow pl-2 pr-1 text-end">2h 33m</div>
     </div>
     <div class="flex border-b-2 border-neutral-600 px-1 py-1 text-white">
-      <div class="px-1">ETA</div>
+      <div class="px-1 text-green-400">ETA</div>
       <div class="flex-grow pl-2 pr-1 text-end">20:45</div>
     </div>
     <div class="flex border-b-2 border-neutral-600 px-1 py-1 text-white">
-      <div class="px-1">Flow</div>
+      <div class="px-1 text-green-400">Flow</div>
       <div class="flex-grow pl-2 pr-1 text-end">60 mm³/s</div>
     </div>
     <div class="flex border-b-2 border-neutral-600 px-1 py-1 text-white">
-      <div class="px-1">Speed</div>
+      <div class="px-1 text-green-400">Speed</div>
       <div class="flex-grow pl-2 pr-1 text-end">80 mm/s</div>
     </div>
   </div>
@@ -161,8 +162,8 @@
   </div>
   <div class="flex flex-grow flex-col flex-wrap items-stretch justify-end gap-1 p-1">
     <button class="flex rounded border-l-4 border-purple-400 bg-neutral-600 px-1 py-2 text-white hover:bg-neutral-500" on:click={() => goto('/toolhead')}>
-      <div class="self-center px-1"><Fa icon={faGear} /></div>
-      <div class="flex-grow pr-1 text-end">Extruder</div>
+      <div class="self-center px-1"><Fa icon={faArrowsUpDownLeftRight} /></div>
+      <div class="flex-grow pr-1 text-end">Toolhead</div>
     </button>
 
     <button class="flex rounded border-l-4 border-fuchsia-400 bg-neutral-600 px-1 py-2 text-white hover:bg-neutral-500" on:click={() => goto('/zoffset')}>
--- src/routes/extrude/+page.svelte
+++ src/routes/extrude/+page.svelte
@@ -7,20 +7,22 @@
     faArrowUp,
     faArrowsUpDownLeftRight,
     faBarsProgress,
+    faDownLong,
     faGear,
     faList,
     faMinus,
     faPause,
     faPlus,
     faSkull,
-    faSquareFull,
+    faSortDown,
+    faSortUp,
     faStop
   } from '@fortawesome/free-solid-svg-icons';
   import Fa from 'svelte-fa';
 
   let stepsArrIdx = 4;
   let stepsArr = [1, 2, 5, 10, 20, 50, 100];
-  let presetArr = [0, 150, 190, 200, 210, 230, 250];
+  let presetArr = [1, 2, 3, 5, 7, 10];
 
   let nozzleTemp = moonraker.extruder.Temperature;
   let nozzleTempTarget = moonraker.extruder.Target;
@@ -73,31 +75,21 @@
   }
 </script>
 
-<div class="flex flex-grow flex-row border-t border-red-400 bg-neutral-800">
-  <div class="flex w-3/4 flex-col items-center justify-around gap-1 p-1">
-    <span class="flex w-full flex-row justify-center">
-      <button class="flex rounded border-l-4 border-red-400 bg-neutral-700 py-2 text-white hover:bg-neutral-500 disabled:text-neutral-500">
-        <div class="self-center px-1"><Fa icon={faArrowDown} /></div>
-        <div class="flex-grow self-center px-1 text-end">Decrease</div>
+<div class="flex flex-grow flex-row border-t border-teal-400 bg-neutral-800">
+  <div class=" flex w-3/4 flex-col items-center justify-around px-1">
+    <span class="flex flex-col justify-center gap-1">
+      <button class="flex rounded border-l-4 border-teal-400 bg-neutral-700 py-2 text-white hover:bg-neutral-500 disabled:text-neutral-500">
+        <div class="self-center px-1"><Fa icon={faSortUp} /></div>
+        <div class="flex-grow self-center px-1 text-end">Retract</div>
       </button>
-      <span class="flex flex-col p-1">
-        <div class="flex flex-row justify-end border-neutral-600 text-white">
-          <p class="px-1 text-red-400">Target</p>
-          <p class="flex-grow pl-2 pr-1 text-end">{$nozzleTempTarget.toFixed(0)} °C</p>
-        </div>
-        <div class="flex flex-row justify-end border-neutral-600 text-white">
-          <p class="px-1 text-white">Current</p>
-          <p class="flex-grow pl-2 pr-1 text-end">{$nozzleTemp.toFixed(0)} °C</p>
-        </div>
-      </span>
-      <button class="flex rounded border-l-4 border-red-400 bg-neutral-700 py-2 text-white hover:bg-neutral-500 disabled:text-neutral-500">
-        <div class="self-center px-1"><Fa icon={faArrowUp} /></div>
-        <div class="flex-grow self-center px-1 text-end">Increase</div>
+      <button class="flex rounded border-l-4 border-teal-400 bg-neutral-700 py-2 text-white hover:bg-neutral-500 disabled:text-neutral-500">
+        <div class="self-center px-1"><Fa icon={faSortDown} /></div>
+        <div class="flex-grow self-center px-1 text-end">Extrude</div>
       </button>
     </span>
     <span class="flex w-full items-center justify-center">
       <div class="flex flex-col gap-1">
-        <p class="flex border-neutral-600 text-sm text-white">Steps in °C</p>
+        <p class="flex border-neutral-600 text-sm text-white">Steps in mm</p>
         <div class="flex flex-row flex-wrap gap-1">
           {#each stepsArr as number, i}
             <button
@@ -113,7 +105,7 @@
     </span>
     <span class="flex w-full items-center justify-center">
       <div class="flex flex-col gap-1">
-        <p class="flex border-neutral-600 text-sm text-white">Preset in °C</p>
+        <p class="flex border-neutral-600 text-sm text-white">Speed in mm/s²</p>
         <div class="flex flex-row flex-wrap gap-1">
           {#each presetArr as number, i}
             <button
@@ -128,11 +120,11 @@
       </div>
     </span>
   </div>
-  <div class="flex w-1/4 flex-col flex-wrap justify-around gap-1 p-1">
+  <div class="flex w-1/4 flex-col flex-wrap justify-around gap-1 px-1">
     <span class="flex flex-col flex-wrap justify-center gap-1">
-      <button class="flex rounded border-l-4 border-blue-400 bg-neutral-700 px-1 py-2 text-white hover:bg-neutral-500" on:click={() => goto('/temp_bed')}>
-        <div class="self-center px-1"><Fa icon={faSquareFull} /></div>
-        <div class="flex-grow pl-2 pr-1 text-end">{$bedTempTarget.toFixed(0)} °C</div>
+      <button class="flex rounded border-l-4 border-red-400 bg-neutral-700 px-1 py-2 text-white hover:bg-neutral-500" on:click={() => goto('/temp_nozzle')}>
+        <div class="self-center px-1"><Fa icon={faDownLong} /></div>
+        <div class="flex-grow self-center pl-2 pr-1 text-end">{$nozzleTempTarget.toFixed(0)} °C</div>
       </button>
 
       <button
@@ -143,22 +135,20 @@
         <div class="flex-grow self-center pl-1 pr-2 text-end">Toolhead</div>
       </button>
     </span>
-    <span class="flex flex-col flex-wrap justify-center gap-1">
-      <button
-        class="flex rounded border-l-4 border-green-400 bg-neutral-700 py-2 text-white hover:bg-neutral-500 disabled:text-neutral-500"
-        on:click={() => goto('/')}
-      >
-        <div class="self-center px-2"><Fa icon={faBarsProgress} /></div>
-        <div class="flex-grow self-center pl-1 pr-2 text-end">Status</div>
-      </button>
-    </span>
+    <button
+      class="flex rounded border-l-4 border-green-400 bg-neutral-700 py-2 text-white hover:bg-neutral-500 disabled:text-neutral-500"
+      on:click={() => goto('/')}
+    >
+      <div class="self-center px-2"><Fa icon={faBarsProgress} /></div>
+      <div class="flex-grow self-center pl-1 pr-2 text-end">Status</div>
+    </button>
     <button
       class="flex rounded border-l-4 border-red-400 bg-neutral-600 px-1 py-2 text-white shadow hover:bg-neutral-500 disabled:text-neutral-500"
       disabled
       on:click={() => emergencyStop()}
     >
       <div class="self-center px-1"><Fa icon={faSkull} /></div>
-      <div class="flex-grow pl-2 pr-1 text-end">Kill</div>
+      <div class="flex-grow self-center pl-2 pr-1 text-end">Kill</div>
     </button>
   </div>
 </div>
