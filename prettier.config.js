4c3d9679fc533670781b555e4723712090324461
FreakyDude <developer@freakydu.de>
2023-10-17 01:08:53 +0200

    Add Tauri Framework
    Playing with design part 4
    
    Signed-off-by: Frank Roth <developer@freakydu.de>

---
+++ prettier.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  plugins: [
+    'prettier-plugin-svelte',
+    'prettier-plugin-organize-imports',
+    'prettier-plugin-tailwindcss',
+    'prettier-plugin-sh',
+    'prettier-plugin-sort-json',
+    'prettier-plugin-toml',
+    'prettier-plugin-toml',
+    'prettier-plugin-rust'
+  ],
+  tailwindFunctions: ['clsx,tw']
+};
