60438823677edafd99e083e74c121a84b1a59c08
FreakyDude <developer@freakydu.de>
2023-10-18 19:00:42 +0200

    Basic reconnect cycle startscreen
    Signed-off-by: Frank Roth <developer@freakydu.de>

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,74 @@
-<script>
+<script lang="ts">
 import { goto } from '$app/navigation';
+import { client, moonraker } from '$lib/base.svelte';
+import { JsonRpcRequest } from '$lib/jsonrpc/types/JsonRpcRequest';
+import { onMount } from 'svelte';
+
+let isFullscreen = false;
+let outerElement: Element;
+
+let isConnected = client.isConnected;
+let klippyState = moonraker.klippyState.state;
+let klippyStateMessage = moonraker.klippyState.message;
+
+async function reconnectToMoonraker() {
+  await moonraker.disconnect();
+  await moonraker.connect();
+}
+
+async function printerRestart() {
+  let stopRequest = new JsonRpcRequest({
+    method: 'printer.restart',
+    params: {}
+  });
+  await client.sendRequest(stopRequest);
+}
+
+async function firmwareRestart() {
+  let stopRequest = new JsonRpcRequest({
+    method: 'printer.firmware_restart',
+    params: {}
+  });
+  await client.sendRequest(stopRequest);
+}
+
+onMount(async () => {
+  // await reconnectToMoonraker();
+});
 </script>
 
-<div class="flex flex-grow flex-col items-center justify-around gap-4 bg-neutral-800">
-  <p class="text-5xl font-bold text-gray-300">Klipper Touch</p>
-  <button class="btn-touch1 border-green-400" on:click="{() => goto('/printstate')}">Status</button>
-  <button class="btn-touch1 border-rose-400" on:click="{() => goto('/test')}">Test</button>
-  <p class="text-2xl font-bold text-red-700">by freakyDude</p>
+<div class="page-dark flex-col flex-wrap" bind:this="{outerElement}">
+  {#if $isConnected}
+    <p class="label1">Moonraker: connected</p>
+
+    {#if $klippyState === 'disconnected'}
+      <p class="label1">Firmware: disconnected</p>
+      <p class="label1">{$klippyStateMessage}</p>
+      <button class="btn-touch1" on:click="{() => reconnectToMoonraker()}">Reconnect</button>
+    {:else if $klippyState === 'startup'}
+      <p class="label1">Firmware: startup</p>
+      <p class="label1">{$klippyStateMessage}</p>
+      <button class="btn-touch1" on:click="{() => printerRestart()}">Restart</button>
+      <button class="btn-touch1" on:click="{() => firmwareRestart()}">Firmware Restart</button>
+    {:else if $klippyState === 'ready'}
+      <p class="label1">Firmware: ready</p>
+      <!-- <p class="label1">{$klippyStateMessage}</p> -->
+      <!-- <button class="btn-touch1" on:click="{() => printerRestart()}">Restart</button> -->
+      <button class="btn-touch1" on:click="{() => goto('/printstate')}">Printer state</button>
+    {:else if $klippyState === 'shutdown'}
+      <p class="label1">Firmware: shutdown</p>
+      <p class="label1">{$klippyStateMessage}</p>
+      <button class="btn-touch1" on:click="{() => printerRestart()}">Restart</button>
+      <button class="btn-touch1" on:click="{() => firmwareRestart()}">Firmware Restart</button>
+    {:else if $klippyState === 'error'}
+      <p class="label1">Firmware: error</p>
+      <p class="label1">{$klippyStateMessage}</p>
+      <button class="btn-touch1" on:click="{() => printerRestart()}">Restart</button>
+      <button class="btn-touch1" on:click="{() => firmwareRestart()}">Firmware Restart</button>
+    {/if}
+  {:else}
+    <p class="label1">Moonraker: disconnected</p>
+    <button class="btn-touch1" on:click="{() => reconnectToMoonraker()}">Reconnect</button>
+    <!-- <button class="btn-touch1" on:click="{() => switchFullscreen()}">Fullscreen</button> -->
+  {/if}
 </div>
