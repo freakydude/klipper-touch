305df7d5ad58d475bc22fb6ec0fabbb37c7916be
Frank Roth <developer@freakydu.de>
2023-10-24 13:32:05 +0200

    Rework Printstate Screen - part2
    Signed-off-by: Frank Roth <developer@freakydu.de>

--- src/routes/printstate/+page.svelte
+++ src/routes/printstate/+page.svelte
@@ -87,19 +87,74 @@
 </script>
 
 <div class="page-dark flex-col items-stretch">
-  <p class="fixed -left-9 top-14 -rotate-90 text-sm text-neutral-100">Standby / Pause</p>
+  <p class="fixed left-1 top-1 text-sm text-neutral-100">Standby / Pause</p>
 
-  <div class="flex flex-grow flex-row gap-3 p-3">
-    <div class="flex flex-col bg-blue-300">
-      <div class="flex bg-neutral-500">Blubb</div>
-      <div class="flex bg-neutral-500">Blubb</div>
-      <div class="flex bg-neutral-500">Blubb</div>
-      <div class="flex bg-neutral-500">Blubb</div>
+  <div class="flex flex-grow flex-row justify-between gap-3 p-1">
+    <div class="flex flex-col items-end justify-center gap-3">
+      <div class="flex flex-col items-end gap-1 rounded-lg bg-neutral-500 px-3 py-2">
+        <table class="table-auto text-sm text-neutral-50">
+          <tr class="border-b border-neutral-800">
+            <td class="pr-2 text-end">Nozzle</td>
+            <td class="text-end">130</td>
+            <td>/</td>
+            <td class="text-end">210</td>
+            <td>°C</td>
+          </tr>
+          <tr>
+            <td class="pr-2 text-end">Bed</td>
+            <td class="text-end">101</td>
+            <td>/</td>
+            <td class="text-end">115 </td>
+            <td>°C</td>
+          </tr>
+        </table>
+      </div>
+      <div class="flex flex-col items-end gap-1 rounded-lg bg-neutral-500 px-3 py-2">
+        <table class="table-auto text-sm text-neutral-50">
+          <tr class="border-b border-neutral-800">
+            <td class="pr-2 text-end">X</td>
+            <td class="text-end">255.8</td>
+            <td>mm</td>
+          </tr>
+          <tr class="border-b border-neutral-800">
+            <td class="pr-2 text-end">Y</td>
+            <td class="text-end">142.3</td>
+            <td>mm</td>
+          </tr>
+          <tr class="border-b border-neutral-800">
+            <td class="pr-2 text-end">Z</td>
+            <td class="text-end">13.1</td>
+            <td>mm</td>
+          </tr>
+          <tr>
+            <td class="pr-2 text-end">Z-Off</td>
+            <td class="text-end">0.133</td>
+            <td>mm</td>
+          </tr>
+        </table>
+      </div>
     </div>
-    <div class="flex flex-grow flex-col bg-green-300">
-      <div class="flex bg-orange-500">Blobb</div>
+    <div class="flex flex-col items-center justify-center gap-3">
+      <div class="flex flex-col items-center gap-1 rounded-lg bg-neutral-500 px-3 py-2">
+        <p class="w-40 overflow-clip text-center text-sm text-neutral-50">Benchy3d-PLA-F1.0-S80</p>
+        <div class="flex h-32 w-32 flex-col items-stretch justify-center rounded-lg bg-orange-400">
+          <p class="text-center">Loaded Print</p>
+          <p class="text-center">Preview Picture</p>
+          <p class="text-center">here</p>
+        </div>
+        <table class="table-auto text-sm text-neutral-50">
+          <tr class="border-b border-neutral-800">
+            <td class="pr-2 text-end">Estimate:</td>
+            <td>3h15 / 20:47</td>
+          </tr>
+          <tr>
+            <td class="pr-2 text-end">Weight:</td>
+            <td>105 g</td>
+          </tr>
+        </table>
+      </div>
     </div>
-    <div class="flex flex-col justify-center gap-3">
+    <div class="flex flex-col items-end justify-center gap-3">
       <button
         class="flex h-14 w-20 items-center justify-center rounded-lg bg-neutral-500 px-3 py-2 font-semibold text-neutral-50 drop-shadow-md hover:bg-neutral-400">
         Load
@@ -125,7 +180,7 @@
       Prep
     </button>
     <div class="flex flex-grow items-end justify-end">
-      <p class="text-sm text-neutral-50">15:32</p>
+      <p class="pb-1 pr-1 text-sm text-neutral-50">15:32</p>
     </div>
     <button class="flex w-16 items-center justify-center rounded-lg bg-neutral-500 px-3 py-2 font-semibold text-red-800 drop-shadow-md hover:bg-neutral-400">
       Kill
