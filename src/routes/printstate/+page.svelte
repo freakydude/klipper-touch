f0763cec098635e60c23b39ce3254c5b068d1f40
Frank Roth <developer@freakydu.de>
2023-10-24 15:58:14 +0200

    Rework Printstate Screen - part2-fine
    Signed-off-by: Frank Roth <developer@freakydu.de>

--- src/routes/printstate/+page.svelte
+++ src/routes/printstate/+page.svelte
@@ -87,10 +87,10 @@
 </script>
 
 <div class="page-dark flex-col items-stretch">
-  <p class="fixed left-1 top-1 text-sm text-neutral-100">Standby / Pause</p>
+  <p class="fixed left-2 top-1 text-sm text-neutral-100">Ready</p>
 
-  <div class="flex flex-grow flex-row justify-between gap-3 p-1">
-    <div class="flex flex-col items-end justify-center gap-3">
+  <div class="flex flex-grow flex-row justify-between gap-3">
+    <div class="flex flex-col items-end justify-center gap-3 p-2 pl-2 pr-1">
       <div class="flex flex-col items-end gap-1 rounded-lg bg-neutral-500 px-3 py-2">
         <table class="table-auto text-sm text-neutral-50">
           <tr class="border-b border-neutral-800">
@@ -134,7 +134,7 @@
         </table>
       </div>
     </div>
-    <div class="flex flex-col items-center justify-center gap-3">
+    <div class="flex flex-col items-center justify-center gap-3 px-1 py-2">
       <div class="flex flex-col items-center gap-1 rounded-lg bg-neutral-500 px-3 py-2">
         <p class="w-40 overflow-clip text-center text-sm text-neutral-50">Benchy3d-PLA-F1.0-S80</p>
         <div class="flex h-32 w-32 flex-col items-stretch justify-center rounded-lg bg-orange-400">
@@ -154,35 +154,43 @@
         </table>
       </div>
     </div>
-    <div class="flex flex-col items-end justify-center gap-3">
+    <div class="flex flex-col items-end justify-center gap-3 py-2 pl-1">
       <button
-        class="flex h-14 w-20 items-center justify-center rounded-lg bg-neutral-500 px-3 py-2 font-semibold text-neutral-50 drop-shadow-md hover:bg-neutral-400">
+        class="flex h-14 w-20 items-center justify-center rounded-l-lg bg-neutral-500 px-3 py-2 font-semibold text-neutral-50 drop-shadow-md hover:bg-neutral-400">
         Load
       </button>
       <button
-        class="flex h-14 w-20 items-center justify-center rounded-lg bg-neutral-500 px-3 py-2 font-semibold text-neutral-50 drop-shadow-md hover:bg-neutral-400">
+        class="flex h-14 w-20 items-center justify-center rounded-l-lg bg-neutral-500 px-3 py-2 font-semibold text-neutral-50 drop-shadow-md hover:bg-neutral-400">
         Start
       </button>
     </div>
   </div>
 
   <div class="flex flex-row gap-x-1 bg-neutral-700 px-1 pb-1">
-    <button class="flex w-16 items-center justify-center rounded-lg bg-neutral-500 px-3 py-2 font-semibold text-neutral-50 drop-shadow-md hover:bg-neutral-400">
+    <button
+      class="flex w-16 items-center justify-center rounded-b-lg bg-neutral-400 px-3 py-2 font-semibold text-neutral-50 drop-shadow-md hover:bg-neutral-400">
+      State
+    </button>
+    <button
+      class="flex w-16 items-center justify-center rounded-b-lg bg-neutral-500 px-3 py-2 font-semibold text-neutral-50 drop-shadow-md hover:bg-neutral-400">
       Move
     </button>
-    <button class="flex w-16 items-center justify-center rounded-lg bg-neutral-500 px-3 py-2 font-semibold text-neutral-50 drop-shadow-md hover:bg-neutral-400">
+    <button
+      class="flex w-16 items-center justify-center rounded-b-lg bg-neutral-500 px-3 py-2 font-semibold text-neutral-50 drop-shadow-md hover:bg-neutral-400">
       Temp
     </button>
-    <button class="flex w-16 items-center justify-center rounded-lg bg-neutral-500 px-3 py-2 font-semibold text-neutral-50 drop-shadow-md hover:bg-neutral-400">
+    <button
+      class="flex w-16 items-center justify-center rounded-b-lg bg-neutral-500 px-3 py-2 font-semibold text-neutral-50 drop-shadow-md hover:bg-neutral-400">
       Baby
     </button>
-    <button class="flex w-16 items-center justify-center rounded-lg bg-neutral-500 px-3 py-2 font-semibold text-neutral-50 drop-shadow-md hover:bg-neutral-400">
+    <button
+      class="flex w-16 items-center justify-center rounded-b-lg bg-neutral-500 px-3 py-2 font-semibold text-neutral-50 drop-shadow-md hover:bg-neutral-400">
       Prep
     </button>
     <div class="flex flex-grow items-end justify-end">
       <p class="pb-1 pr-1 text-sm text-neutral-50">15:32</p>
     </div>
-    <button class="flex w-16 items-center justify-center rounded-lg bg-neutral-500 px-3 py-2 font-semibold text-red-800 drop-shadow-md hover:bg-neutral-400">
+    <button class="flex w-16 items-center justify-center rounded-b-lg bg-neutral-500 px-3 py-2 font-semibold text-red-800 drop-shadow-md hover:bg-neutral-400">
       Kill
     </button>
   </div>
